8b069f1847efcefa704fe76d813198b0cf8f7044
Jacinto Leandro Rodrigues Pereira <famtrooper@gmail.com>
2026-05-08 07:15:11 +0100

    button test adjusted to test h1 elements

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -17,6 +17,9 @@ describe ("DOM tests", () => {
         buttonClick();
         expect (document.getElementById("par").innerHTML).toEqual("You Clicked!");
     });
+    test ("h1 should exist",()=> {
+        expect (document.getElementsByTagName("h1").length).toBe(1);
+    });
 
 
 });
