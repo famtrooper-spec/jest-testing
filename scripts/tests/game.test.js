bd6d8caffb5da6e7f745099bb760629159e37191
Jacinto Leandro Rodrigues Pereira <famtrooper@gmail.com>
2026-05-09 07:35:25 +0100

    tests and code built for newGame ans show Score

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,4 +1,4 @@
-const { game } = require("../game.js");
+const { game, newGame, showScore } = require("../game");
 
 beforeAll(() => { 
 let fs = require("fs");
@@ -12,4 +12,38 @@ describe(" game object contains correct keys",() => {
     test ("Score key exists", () => {
         expect("score"in game).toBe (true);
 });
+test ("currentGame key exists", () => {
+        expect("currentGame"in game).toBe (true);
+});
+test ("playerMove key exists", () => {
+        expect("playerMove"in game).toBe (true);
+});
+test ("choices key exists", () => {
+        expect("choices"in game).toBe (true);
+});
+test ("choices contains correct ids", () => {
+        expect(game.choices).toEqual (["button1", "button2", "button3", "button4"]);
+});
 })
+
+describe(" newGame works correctly",() => {
+  beforeAll(() => {
+    game.score = 42;
+    game.playerMove = ["button1", "button2"];
+    game.curentGame = ["button1", "button2",];
+    document.getElementById("score").innerText = "42";
+    newGame();
+  });
+  test ("should set game score to zero", () => {
+        expect(game.score).toEqual (0);
+  });
+  test ("should set playerMove to 0", () => {
+        expect(game.playerMove).toEqual ([]);
+  });
+   test ("should set currentGame to 0", () => {
+        expect(game.currentGame).toEqual ([]);
+  });
+  test ("should display 0 for the element with the id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual ("0");
+  });
+});
